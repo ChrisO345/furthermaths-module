cdb9519db62787a72a1ef8084d5b3111fac0dc57
Chris Oliver <46587501+ChrisO275@users.noreply.github.com>
2022-08-07 23:06:31 +1200

    added lcm and gcd functions

--- furthermaths/prime_factors.py
+++ furthermaths/prime_factors.py
@@ -18,3 +18,50 @@ def prime_factors(n):
     if n > 1:
         factors.append(n)
     return factors
+
+
+# TODO extend to n arguments
+def lcm(a, b):
+    """
+    Returns the lowest common multiple of a and b
+    :param a:
+    :param b:
+    :return:
+    """
+    _a = prime_factors(a)
+    _b = prime_factors(b)
+    _c = []
+    for i in _a:
+        _c.append(i)
+        if i in _b:
+            _b.remove(i)
+    _c.extend(_b)
+    total = 1
+    for j in _c:
+        total *= j
+    return total
+
+
+def gcd(a, b):
+    """
+    Returns the greatest common denominator of a and b
+    :param a:
+    :param b:
+    :return:
+    """
+    _a = prime_factors(a)
+    _b = prime_factors(b)
+    _c = []
+    for i in _a:
+        if i in _b:
+            _b.remove(i)
+            _c.append(i)
+    total = 1
+    for j in _c:
+        total *= j
+    return total
+
+
+if __name__ == '__main__':
+    print(lcm(15, 10))
+    print(gcd(15, 10))
