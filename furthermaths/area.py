57059bd489a280989c83e84292dfe1c9d323c08b
Chris Oliver <46587501+ChrisO275@users.noreply.github.com>
2022-08-09 21:59:42 +1200

    area functions

--- furthermaths/area.py
+++ furthermaths/area.py
@@ -10,6 +10,8 @@ def area_rect(w: int | float, h: int | float):
     :param h: height of shape
     :return:
     """
+    if w < 0 or h < 0:
+        raise ValueError("area_rect() only accepts non-negative values")
     return w * h
 
 
@@ -20,18 +22,21 @@ def area_square(a: int | float):
     :param a: length of shape
     :return:
     """
+    if a < 0:
+        raise ValueError("area_square() only accepts non-negative values")
     return a**2
 
 
 def area_circle(r: int | float):
-    from .constants import pi
     """
     Finds the area of circle
     
     :param r: radius of shape
     :return: 
     """
-    return pi * r**2
+    if r < 0:
+        raise ValueError("area_circle() only accepts non-negative values")
+    return 3.141592653589793 * r**2
 
 
 def area_trapezium(a: int | float, b: int | float, h: int | float):
@@ -43,6 +48,8 @@ def area_trapezium(a: int | float, b: int | float, h: int | float):
     :param h: height of shape
     :return:
     """
+    if a < 0 or b < 0 or h < 0:
+        raise ValueError("area_trapezium() only accepts non-negative values")
     return (a + b) * h / 2
 
 
@@ -55,6 +62,8 @@ def area_triangle(a: int | float, b: int | float = 0, c: int | float = 0):
     :param c: side 3 of shape
     :return:
     """
+    if a < 0 or b < 0 or c < 0:
+        raise ValueError("area_triangle() only accepts non-negative values")
     if b == 0:
         b, c = a
     s = (a + b + c) / 2
@@ -72,6 +81,8 @@ def area_pentagon(a: int | float, b: int | float, c: int | float, d: int | float
     :param e: side 5 of shape
     :return:
     """
+    if a < 0 or b < 0 or c < 0 or d < 0 or e < 0:
+        raise ValueError("area_pentagon() only accepts non-negative values")
     s = (a + b + c + d + e) / 2
     return (s * (s - a) * (s - b) * (s - c) * (s - d) * (s - e)) ** 0.5
 
@@ -84,6 +95,8 @@ def area_regular_polygon(n: int, a: int | float):
     :param a: length of shape
     :return:
     """
+    if n < 0 or a < 0:
+        raise ValueError("area_regular_polygon() only accepts non-negative values")
     return n * a**2 / (4 * tan(180 / n))
 
 
@@ -95,7 +108,35 @@ def area_ellipse(w: int | float, h: int | float):
     :param h: height of shape
     :return:
     """
+    if w < 0 or h < 0:
+        raise ValueError("area_circle() only accepts non-negative values")
     return area_circle(w / 2) * h
+
+
+def area_parallelogram(base: float, height: float) -> float:
+    """
+    Finds the area of parallelogram
+
+    :param base:
+    :param height:
+    :return:
+    """
+    if base < 0 or height < 0:
+        raise ValueError("area_parallelogram() only accepts non-negative values")
+    return base * height
+
+
+def area_rhombus(diagonal_1: float, diagonal_2: float) -> float:
+    """
+    Finds the area of rhombus
+
+    :param diagonal_1:
+    :param diagonal_2:
+    :return:
+    """
+    if diagonal_1 < 0 or diagonal_2 < 0:
+        raise ValueError("area_rhombus() only accepts non-negative values")
+    return 1 / 2 * diagonal_1 * diagonal_2
 
 
 if __name__ == '__main__':
--- furthermaths/surface_area.py
+++ furthermaths/surface_area.py
@@ -1,7 +1,7 @@
 """Surface Area of 3d shapes"""
 
 
-def surface_cuboid(w: int | float, h: int | float, d: int | float) -> float:
+def surface_area_cuboid(w: int | float, h: int | float, d: int | float) -> float:
     """
     Finds the surface area of a cuboid
 
@@ -10,11 +10,12 @@ def surface_cuboid(w: int | float, h: int | float, d: int | float) -> float:
     :param d: depth of shape
     :return:
     """
+    if w < 0 or h < 0 or d < 0:
+        raise ValueError("surface_area_cuboid() only accepts non-negative values")
     return 2 * (w * h + w * d + h * d)
 
 
-def surface_cylinder(r: int | float, h: int | float) -> float:
-    from .constants import pi
+def surface_area_cylinder(r: int | float, h: int | float) -> float:
     """
     Finds the surface area of a cylinder
     
@@ -22,31 +23,36 @@ def surface_cylinder(r: int | float, h: int | float) -> float:
     :param h: height of shape
     :return: 
     """
-    return 2 * pi * r * h + 2 * pi * r**2
+    if r < 0 or h < 0:
+        raise ValueError("surface_area_cylinder() only accepts non-negative values")
+    return 2 * 3.141592653589793 * r * h + 2 * 3.141592653589793 * r**2
 
 
-def surface_cube(a: int | float) -> float:
+def surface_area_cube(a: int | float) -> float:
     """
     Finds the surface area of a cube
 
     :param a: length of shape
     :return:
     """
+    if a < 0:
+        raise ValueError("surface_area_cube() only accepts non-negative values")
     return 6 * a**2
 
 
-def surface_sphere(r: int | float) -> float:
-    from .constants import pi
+def surface_area_sphere(r: int | float) -> float:
     """
     Finds the surface area of a sphere
     
     :param r: radius of shape
     :return: 
     """
-    return 4 * pi * r**2
+    if r < 0:
+        raise ValueError("surface_area_sphere() only accepts non-negative values")
+    return 4 * 3.141592653589793 * r**2
 
 
-def surface_pyramid_square(a: int | float, h: int | float) -> float:
+def surface_area_pyramid_square(a: int | float, h: int | float) -> float:
     """
     Finds the surface area of a square pyramid
 
@@ -54,10 +60,12 @@ def surface_pyramid_square(a: int | float, h: int | float) -> float:
     :param h: perpendicular height of shape
     :return:
     """
+    if a < 0 or h < 0:
+        raise ValueError("surface_area_pyramid_square() only accepts non-negative values")
     return a**2 + 4 * a * h / 2
 
 
-def surface_pyramid_triangle(a: int | float, h: int | float) -> float:
+def surface_area_pyramid_triangle(a: int | float, h: int | float) -> float:
     """
     Finds the surface area of a square pyramid
 
@@ -65,7 +73,34 @@ def surface_pyramid_triangle(a: int | float, h: int | float) -> float:
     :param h: perpendicular height of shape
     :return:
     """
+    if a < 0 or h < 0:
+        raise ValueError("surface_area_pyramid_triangle() only accepts non-negative values")
     return 4 * a * h / 2
+
+
+def surface_area_hemisphere(radius: float) -> float:
+    """
+    Finds the surface area of a hemisphere
+
+    :param radius:
+    :return:
+    """
+    if radius < 0:
+        raise ValueError("surface_area_hemisphere() only accepts non-negative values")
+    return 3 * 3.141592653589793 * radius**2
+
+
+def surface_area_cone(radius: float, height: float) -> float:
+    """
+    Finds the surface area of a cone
+
+    :param radius:
+    :param height:
+    :return:
+    """
+    if radius < 0 or height < 0:
+        raise ValueError("surface_area_cone() only accepts non-negative values")
+    return 3.141592653589793 * radius * (radius + (height**2 + radius**2) ** 0.5)
 
 
 # TODO: function for a nth based pyramid, nth based prism, ovular cylinder, cone, ovular cone, ovular torus, torus,
