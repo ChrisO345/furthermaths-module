f97ad7c8afde421e2609392a7b14babb61e76243
Chris O <46587501+ChrisO345@users.noreply.github.com>
2022-08-10 10:30:10 +1200

    Create gamma.py

---
+++ furthermaths/complex/gamma.py
@@ -0,0 +1,15 @@
+"""Gamma Function"""
+
+
+def gamma(x: complex) -> complex:
+  """
+  Returns the gamma function of x
+  
+  :param x:
+  :returns:
+  """
+  pass
+
+
+if __name__ == "__main__":
+  pass
