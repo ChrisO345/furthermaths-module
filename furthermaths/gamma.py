1f2d9d4e089b5880fcba63f41659c200dcc494be
h3athen <gurungaryan73@gmail.com>
2022-10-23 20:14:34 +0530

    ✨ Added Gamma Function

--- furthermaths/gamma.py
+++ furthermaths/gamma.py
@@ -1,16 +1,19 @@
 """Gamma Function"""
 
+def factorial(n):
+    return 1 if (n==1 or n==0) else n * factorial(n - 1)
 
 def gamma(n):
     """
     Returns the gamma function of n.
 
-    :param n: The value to find the gamma function of.
+    :param n: The value to find the gamma function of (non-complex/positive whole number)
     :return: The gamma function of n.
     """
-    pass
-
-
-# TODO research and complete gamma function
+    if n > 0 and isinstance(n, int): #checks if the number is positive and whole
+        return factorial(n-1)
+    
 if __name__ == '__main__':
-    pass
+    print(gamma(5))
+    print(gamma(11))
+    print(gamma(25))
