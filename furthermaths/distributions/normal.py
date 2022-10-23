10bbeb2e46241e6ca1c4022ef357343c807b668e
HridayAg0102 <hridayagrawal0102@gmail.com>
2022-10-23 15:52:20 +0530

    updated the normal distribution file

--- furthermaths/distributions/normal.py
+++ furthermaths/distributions/normal.py
@@ -1,25 +1,34 @@
 """Normal Distribution"""
 
 
-# TODO fix this
+def normal(mu: int or float, variance: int or float, x: int or float) -> float:
+    """
+    :param mu: mean of the data
+    :param variance: variance of the data
+    :returns: -> the value of Normal Probability Distribution Function at given "x".
+              -> The value of p.d.f depends on "x", "mu", "variance" and "sd"
+    """
+    if variance < 0:
+        raise ValueError("variance must be a positive number")
+    sd = variance ** 0.5  # standard deviation
+    e = 2.71828182846
+    pi = 3.14159265359
+    return (1/(variance*2*pi) ** 0.5) * e ** (-0.5 * (x-mu/sd)**2)
 
-# def normal(mu: int | float, variance: int | float, x: int | float) -> float:
-#     """
-#     Returns the normal distribution of a probability and its variance
 
-#     :param mu:
-#     :param var:
-#     :returns:
-#     """
-#     if not 1 >= mu >= 0:
-#         raise ValueError("mu must be a positive integer below 1")
-#     if variance < 0:
-#         raise ValueError("variance must be a positive integer")
-#     sd = variance ** 0.5
-#     e = 2.71828182846
-#     pi = 3.14159265359
-#     return (1/(sd*(2*pi)))*e**(-0.5*((x-mu)/sd)**2)
+def zscore(mu: int or float, variance: int or float, x: int or float) -> float:
+    """
+    :param mu: mean of the data
+    :param variance: variance of the data
+    :param x: datapoint for which Z-score is to be calculated
+    :return: the Z-score for the corresponding "x"
+    """
+    if variance < 0:
+        raise ValueError("variance must be a positive number")
+    sd = variance**0.5
+    return (x - mu)/sd
 
 
 if __name__ == "__main__":
-    pass
+    print(normal(3.3, 9.99, 3))
+    print(zscore(3.3, 9.99, 3))
