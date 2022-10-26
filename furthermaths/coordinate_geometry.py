9123d9b14cc5068ac8748a6f88bdcf096a1d2e46
Kaartik Nayak <kaartiknayak2001@gmail.com>
2022-10-26 10:27:38 +0530

    updated according to the changes requested

--- furthermaths/coordinate_geometry.py
+++ furthermaths/coordinate_geometry.py
@@ -1,31 +1,30 @@
 # co ordinate geometry
-from .square_root import sqrt
 
 
-def get_distance_from_X_axis(x:int,y:int)->int:
+def distance_from_X_axis(x:int,y:int)->int:
     """
     Gives distance of given from x-axis
     """
     return y
 
-def get_distance_from_Y_axis(x:int,y:int)->int:
+def distance_from_Y_axis(x:int,y:int)->int:
     """
     Gives distance of given from y-axis
     """
     return x
 
-def get_distance_from_origin(x:int,y:int)->int:
+def distance_from_origin(x:int,y:int)->int:
     """
     Gives distance of given from origin (0,0)
     """
-    return sqrt(x**2+y**2)
+    return (x**2+y**2)**0.5
 
 def distance_between_points(x:list,y:list)->int:
     # x and y should be lists with 2 values only
     """
     Gives distance between given 2 points
     """
-    return sqrt((x[0]-y[0])**2+(x[1]-y[1])**2)
+    return ((x[0]-y[0])**2+(x[1]-y[1])**2)**0.5
 
 def get_slope(x:list,y:list)->int:
     # x =[x1,y1] and y =[x2,y2]
