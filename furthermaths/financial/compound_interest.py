468d27e221c93940e44720e9b8750910a641c1e9
SaumitraPatil <saumitrapatil2703@gmail.com>
2022-10-26 13:11:36 +0530

    added compound interest formula

---
+++ furthermaths/financial/compound_interest.py
@@ -0,0 +1,13 @@
+""" Compound Interest """
+
+def compound_interest(p: int | float, r: int | float , t: int | float):
+    """
+    Finds Compound Interest per annum
+    :param p: principal
+    :param r: rate of interest per annum
+    :param t: time in years
+    """
+    return p*(pow((1+r/100),t))-p
+
+if __name__== '__main__':
+    print(compound_interest(1000, 2, 5))
